11228ffc2146aba3bc1e0f0dda8c5399a2364094
Runa457 <lshdo0426@naver.com>
2025-10-22 20:52:11 +0900

    Version 0.4.1: Increased vanilla wagon max speed

--- prototypes/entity/cargo-wagon.lua
+++ prototypes/entity/cargo-wagon.lua
@@ -15,7 +15,7 @@ local items = {
         weight = 800,
         friction_force = 0.25,
         shortname = "cw-light1",
-        max_speed = 3.0,
+        max_speed = 6.0,
         air_resistance = 0.015,
         color = {r = 0.43, g = 0.63, b = 0.23, a = 1},
         inventory_size = 40
@@ -26,7 +26,7 @@ local items = {
         weight = 500,
         friction_force = 0.25,
         shortname = "cw-light2",
-        max_speed = 3.0,
+        max_speed = 6.0,
         air_resistance = 0.015,
         color = {r = 0.23, g = 0.43, b = 0, a = 1},
         inventory_size = 30
@@ -37,7 +37,7 @@ local items = {
         weight = 400,
         friction_force = 0.25,
         shortname = "cw-light3",
-        max_speed = 3.0,
+        max_speed = 6.0,
         air_resistance = 0.015,
         color = {r = 0, g = 0.28, b = 0, a = 1},
         inventory_size = 30
@@ -48,7 +48,7 @@ local items = {
         weight = 1500,
         friction_force = 0.75,
         shortname = "cw-heavy1",
-        max_speed = 3.0,
+        max_speed = 6.0,
         air_resistance = 0.005,
         color = {r = 0.23, g = 0.43, b = 0.63, a = 1},
         inventory_size = 60
@@ -59,7 +59,7 @@ local items = {
         weight = 2000,
         friction_force = 0.75,
         shortname = "cw-heavy2",
-        max_speed = 3.0,
+        max_speed = 6.0,
         air_resistance = 0.005,
         color = {r = 0, g = 0.23, b = 0.43, a = 1},
         inventory_size = 80
@@ -70,7 +70,7 @@ local items = {
         weight = 2500,
         friction_force = 0.75,
         shortname = "cw-heavy3",
-        max_speed = 3.0,
+        max_speed = 6.0,
         air_resistance = 0.005,
         color = {r = 0, g = 0, b = 0.28, a = 1},
         inventory_size = 100
@@ -84,3 +84,5 @@ for _, item in pairs(items) do
 end
 
 data:extend(cw_list)
+
+data.raw['cargo-wagon']['cargo-wagon'].max_speed = 6.0
--- prototypes/entity/fluid-wagon.lua
+++ prototypes/entity/fluid-wagon.lua
@@ -15,7 +15,7 @@ local items = {
         weight = 800,
         friction_force = 0.25,
         shortname = "fw-light1",
-        max_speed = 3.0,
+        max_speed = 6.0,
         air_resistance = 0.015,
         color = {r = 0.43, g = 0.63, b = 0.23, a = 0.5},
         capacity = 50000
@@ -26,7 +26,7 @@ local items = {
         weight = 500,
         friction_force = 0.25,
         shortname = "fw-light2",
-        max_speed = 3.0,
+        max_speed = 6.0,
         air_resistance = 0.015,
         color = {r = 0.23, g = 0.43, b = 0, a = 0.5},
         capacity = 37500
@@ -37,7 +37,7 @@ local items = {
         weight = 400,
         friction_force = 0.25,
         shortname = "fw-light3",
-        max_speed = 3.0,
+        max_speed = 6.0,
         air_resistance = 0.015,
         color = {r = 0, g = 0.28, b = 0, a = 0.5},
         capacity = 37500
@@ -48,7 +48,7 @@ local items = {
         weight = 1500,
         friction_force = 0.75,
         shortname = "fw-heavy1",
-        max_speed = 3.0,
+        max_speed = 6.0,
         air_resistance = 0.005,
         color = {r = 0.23, g = 0.43, b = 0.63, a = 0.5},
         capacity = 75000
@@ -59,7 +59,7 @@ local items = {
         weight = 2000,
         friction_force = 0.75,
         shortname = "fw-heavy2",
-        max_speed = 3.0,
+        max_speed = 6.0,
         air_resistance = 0.005,
         color = {r = 0, g = 0.23, b = 0.43, a = 0.5},
         capacity = 100000
@@ -70,7 +70,7 @@ local items = {
         weight = 2500,
         friction_force = 0.75,
         shortname = "fw-heavy3",
-        max_speed = 3.0,
+        max_speed = 6.0,
         air_resistance = 0.005,
         color = {r = 0, g = 0, b = 0.28, a = 0.5},
         capacity = 125000
@@ -84,3 +84,5 @@ for _, item in pairs(items) do
 end
 
 data:extend(fw_list)
+
+data.raw['fluid-wagon']['fluid-wagon'].max_speed = 6.0
